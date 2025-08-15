c3ebe2f9e272f0c05178168d48dbfd34e0eba83d
Golumaster <golumaster01@gmail.com>
2025-08-15 23:16:50 +0530

    .

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio/', // Fixed to match GitHub repository name
+  base: '/Portfolio', 
   build: {
     outDir: 'dist',
     rollupOptions: {
